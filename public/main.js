fbeafba3396faeae28ba9ea40ba52f68a40c8aef
M Rumman Hasan <mrummanhasan@gmail.com>
2018-08-28 12:18:20 +0500

    second panel completed

--- public/main.js
+++ public/main.js
@@ -97,10 +97,10 @@ document.getElementById("mainBody").style.marginRight = "40px";
 document.getElementById("mySidenavThree").style.width = "0";
 function openNavThree() {
     // closing 4
-    // document.getElementById("mySidenavFour").style.width = "0";
-    // var w = document.getElementById("mySidenavFour").style.width;
-    // console.log("closing " + w);
-    // document.getElementById("mainBody").style.marginLeft = "40px";
+    document.getElementById("mySidenavFour").style.width = "0";
+    var w = document.getElementById("mySidenavFour").style.width;
+    console.log("closing4 " + w);
+    document.getElementById("mainBody").style.marginRight = "40px";
 
     // opening 3
     document.getElementById("mySidenavThree").style.width = "250px";
@@ -137,5 +137,50 @@ function hideUnhideThree() {
         closeNavThree();
     }else if (marginRight == '40px') {
         dockNavThree();
+    }
+}
+
+////////////////////// SIDE NAVBAR Four ////////////////////////////
+document.getElementById("mySidenavFour").style.width = "0";
+function openNavFour() {
+    // closing 3rd
+    document.getElementById("mySidenavThree").style.width = "0";
+    var w = document.getElementById("mySidenavThree").style.width;
+    console.log("closing3 for 4 " + w);
+    document.getElementById("mainBody").style.marginRight = "40px";
+
+    // opening four
+    document.getElementById("mySidenavFour").style.width = "250px";
+    document.getElementById("mainBody").style.marginLeft = "40px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
+    document.getElementById("openbtn").style.display = "block";
+}
+function closeNavFour() {
+    document.getElementById("mySidenavFour").style.width = "0";
+    var w = document.getElementById("mySidenavFour").style.width;
+    console.log("closing4 " + w);
+    document.getElementById("mainBody").style.marginRight = "40px";
+}
+function toggleNavFour() {
+    var w = document.getElementById('mySidenavFour').style.width;
+    if (w == '0px')
+        openNavFour();
+    else
+        closeNavFour();
+}
+
+function dockNavFour() {
+    console.log("dock4 dock4 dock4");
+    document.getElementById("mainBody").style.marginRight = "289px";
+    // document.getElementById("imgName").src=".../res/unhideIcon.png";
+}
+function hideUnhideFour() {
+    var t = document.getElementById('mainBody').style.marginRight;
+    console.log(t);
+    if (t == '40px') {
+        dockNavFour();
+    }
+    else {
+        closeNavFour();
     }
 }
